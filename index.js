2b37ef9136ec8c204dc3e3d98d842c507a9eb21a
ljvefsurvivalgame <52921946+ljvefsurvivalgame@users.noreply.github.com>
2019-12-28 15:10:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+Bot.on('message', message => {
+if(message.content === "!roll"){
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+    var nbrRandom = getRandomInt(%max numbers%);
+    if(nbrRandom == %replace%){
+        message.reply("Test 1")
+    } else {
+        message.reply("Test 2")
+    }
 }
-var nbrRandom = getRandomInt(%max nombers%);
-if(nbrRandom == 0){
-    console.log("Je suis triste")
-} else {
-     console.log("Je vais bien")
-}
-console.log(nbrRandom);
+})
